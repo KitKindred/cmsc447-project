1e5ce0a6f888e67341e2689b0f3b0b4a8561d812
pthomp1 <47375683+pthomp1@users.noreply.github.com>
2019-04-09 22:34:23 -0400

    added gui package directory
    created a proof-of-concept gui with prototype functionality

---
+++ src/gui/Main.java
@@ -0,0 +1,29 @@
+package gui;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	  @Override
+	    public void start(Stage primaryStage) {
+	        try {
+	        	
+	        	String path="/gui/sample.fxml";
+	            // Read file fxml and draw interface.
+	            Parent root = FXMLLoader.load(getClass().getResource(path));
+	 
+	            primaryStage.setTitle("My Application");
+	            primaryStage.setScene(new Scene(root));
+	            primaryStage.show();
+	         
+	        } catch(Exception e) {
+	            e.printStackTrace();
+	     }
+	 }
+	    
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
