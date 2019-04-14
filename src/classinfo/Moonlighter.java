a235d273d4c0745d792b1eb300e7284667cfdcd2
pthomp1 <47375683+pthomp1@users.noreply.github.com>
2019-04-14 01:42:15 -0400

    stuff
    refactored filesystem packages, added file stuff, accessors, gui population

---
+++ src/classinfo/Moonlighter.java
@@ -0,0 +1,7 @@
+package classinfo;
+public class Moonlighter extends Profession {
+
+    public Moonlighter(int id, int type, String name){
+        super(id, type, name);
+    }
+}
---
+++ src/gui/guiStarter.java
@@ -0,0 +1,32 @@
+package gui;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import classinfo.*;
+
+public class guiStarter extends Application {
+	  @Override
+	    public void start(Stage primaryStage) {
+	        try {
+	        	
+	        	String path="/gui/sample.fxml";
+	            // Read file fxml and draw interface.
+	            Parent root = FXMLLoader.load(getClass().getResource(path));
+	 
+	            primaryStage.setTitle("My Application");
+	            primaryStage.setScene(new Scene(root));
+	            primaryStage.show();
+	         
+	        } catch(Exception e) {
+	            e.printStackTrace();
+	     }
+	 }
+	public static void go(String[] args) {launch(args);}
+	/*
+	public static void main(String[] args) {
+		launch(args);
+	}*/
+}
