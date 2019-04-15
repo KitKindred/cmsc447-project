163ca5c51d7b187e965e2ec5af71442f7ed19bb5
KitKindred <kit@retroscape.com>
2019-04-15 12:35:26 -0400

    Cleanup

--- src/Intern.java
+++
@@ -1,6 +0,0 @@
-public class Intern extends Profession {
-
-    public Intern(int id, int type, String name){
-        super(id, type, name);
-    }
-}
--- src/Moonlighter.java
+++
@@ -1,6 +0,0 @@
-public class Moonlighter extends Profession {
-
-    public Moonlighter(int id, int type, String name){
-        super(id, type, name);
-    }
-}
