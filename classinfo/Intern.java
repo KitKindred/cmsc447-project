864836e12eb3a10390ab2210eaa3eddc6b454f6f
KitKindred <kit@retroscape.com>
2019-04-16 20:06:26 -0400

    Removes duplicate files

--- classinfo/Intern.java
+++
@@ -1,7 +0,0 @@
-package classinfo;
-public class Intern extends Profession {
-
-    public Intern(int id, int type, String name){
-        super(id, type, name);
-    }
-}
--- sysfiles/options.java
+++
@@ -1,5 +0,0 @@
-package sysfiles;
-
-public class options {
-
-}
